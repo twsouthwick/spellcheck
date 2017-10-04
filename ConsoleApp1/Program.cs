8ab1421ab603ff1f9213b778742cefac3af1fff3
Taylor Southwick <tasou@microsoft.com>
2017-10-04 10:43:29 -0700

    Add WPF

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        private static Lazy<bool> _isSpellCheckSupported = new Lazy<bool>(() =>
+        {
+            var type = Type.GetTypeFromCLSID(Guid.Parse("7AB36653-1796-484B-BDFA-E74F1DB7C1DC"), false);
+
+            return type != null;
+        }, true);
+
+        public static bool IsSpellCheckSupported => _isSpellCheckSupported.Value;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Spell check supported: {IsSpellCheckSupported}");
+            Console.ReadLine();
         }
     }
 }
